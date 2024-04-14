c0e05ff92370d4380e4fccb5130ae383f3a6fae2
Jeremy <i2317181@continental.edu.pe>
2024-04-14 18:29:01 -0500

    the better

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,10 @@ module.exports = {
         'color-main':'#1f2b6c',
         'color-secundary':'#159be7',
         'color-terciario':'#bbcef3',
-        'color-cuarto':'#f7f9f9'
+        'color-cuarto':'#f7f9f9',
+        'color-letter':'#202023',
+        'color-red':'#FE5F55',
+        'color-green':'#35FF69'
       },
       container:{
         'padding':'1.75rem'
